eb5f30818fcfee41276d0d72c5ce28752734a5ff
sunniraleigh <sraleigh@smith.edu>
2023-01-17 16:18:40 -0500

    layout pseudocode instructions

---
+++ ticTacToe.py
@@ -0,0 +1,26 @@
+# A terminal-played tic tac toe game
+# Written by @sunniraleigh
+# To play: run `python3 ticTacToe.py`
+
+# define or clear board
+# update list of empty slots
+# determine if there is 1 player or 2 players
+
+# GAMEPLAY
+
+# ask if ready to play
+# run game
+
+  # player 1 turn
+  # check for player 1 win
+    # check if player 2 is person or computer
+      # player 2 turn
+      # check for player 2 turn
+  # check for draw
+
+# GAMEPLAY FUNCTIONS
+# player turn, input player, no output, updates board
+# check for open slot
+# check for win
+# check for draw
+# update board
